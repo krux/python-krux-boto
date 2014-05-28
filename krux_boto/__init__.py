f6fbb0cefbd119806ce47b23dfe09532d07b7556
Jos Boumans <jos@dwim.org>
2014-05-28 10:19:07 -0700

    * setting regions is Hard. the work around is to make it available as a property :(

--- krux_boto/__init__.py
+++ krux_boto/__init__.py
@@ -13,6 +13,10 @@ import os
 
 ### The boto library
 import boto
+import boto.ec2
+
+from boto.ec2 import get_region
+
 
 ### for the application class
 import krux.cli
@@ -24,18 +28,16 @@ from krux.cli           import get_parser, get_group
 
 
 ### Constants
-
-ACCESS_KEY  = 'AWS_ACCESS_KEY_ID'
-SECRET_KEY  = 'AWS_SECRET_ACCESS_KEY'
-NAME        = 'krux-boto'
+ACCESS_KEY      = 'AWS_ACCESS_KEY_ID'
+SECRET_KEY      = 'AWS_SECRET_ACCESS_KEY'
+NAME            = 'krux-boto'
+DEFAULT_REGION  = 'us-east-1'
 
 ### Designed to be called from krux.cli, or programs inheriting from it
 def add_boto_cli_arguments(parser):
 
     group = get_group(parser, 'boto')
 
-    ### All our jobs only use minute & hour at this point
-    ### so don't bother exposing more than that
     group.add_argument(
         '--boto-log-level',
         default = DEFAULT_LOG_LEVEL,
@@ -55,17 +57,39 @@ def add_boto_cli_arguments(parser):
         help    = 'AWS Secret Key to use. Defaults to ENV[%s]' % SECRET_KEY,
     )
 
-class Boto(object):
+    group.add_argument(
+        '--boto-region',
+        default = DEFAULT_REGION,
+        choices = [r.name for r in boto.ec2.regions()],
+        help    = 'EC2 Region to connect to. (default: %(default)s)',
+    )
 
+
+class Boto(object):
     def __init__(self, logger = None, stats = None, parser = None):
 
-        self.name   = NAME
-        self.logger = logger or get_logger(self.name)
-        self.stats  = stats  or get_stats( prefix      = self.name)
-        self.parser = parser or get_parser(description = self.name)
+
+        ### Because we're wrapping boto directly, use ___ as a prefix for
+        ### all our variables, so we don't clash with anything public
+        self.___name   = NAME
+        self.___logger = logger or get_logger(self.___name)
+        self.___stats  = stats  or get_stats( prefix      = self.___name)
+        self.___parser = parser or get_parser(description = self.___name)
 
         ### in case we got some of the information via the CLI
-        self.args       = self.parser.parse_args()
+        self.___args    = self.___parser.parse_args()
+
+        ### this has to be 'public', so callers can use it. It's unfortunately
+        ### near impossible to transparently wrap this, because the boto.config
+        ### is initialized before we get here, and all the classes do a look up
+        ### at compile time. So overriding doesn't help.
+        ### Wrapping doesn't work cleanly, because we 1) would have to wrap
+        ### everything, including future features we can't know about yet, as
+        ### well as 2) poke into the guts of the implementation classes to figure
+        ### out connection strings etc. It's quite cumbersome.
+        ### So for now, we just store the region that was asked for, and let the
+        ### caller use it. See the sample app for a howto.
+        self.cli_region = self.___args.boto_region
 
         ### if these are set, make sure we set the environment again
         ### as well; that way the underlying boto calls will just DTRT
@@ -76,14 +100,14 @@ class Boto(object):
         }
 
         for name, env_var in map.iteritems():
-            val = getattr( self.args, name, None )
+            val = getattr( self.___args, name, None )
 
             if val is not None:
 
                 ### this way we can tell what credentials are being used,
                 ### without dumping the whole secret into the logs
                 pp_val = val[0:3] + '[...]' + val[-3:] if len(val) else '<empty>'
-                self.logger.debug(
+                self.___logger.debug(
                     'Setting boto credential %s to %s', env_var, pp_val
                 )
 
@@ -97,13 +121,13 @@ class Boto(object):
             ### $ FOO= ./myprog.py
             ### It'll return an empty string, and we'd not catch it.
             if not os.environ.get( env_var, None ):
-                self.logger.info(
+                self.___logger.info(
                     'Boto environment credential %s NOT explicitly set ' +
                     '-- boto will look for a .boto file somewhere', env_var
                 )
 
         ### This sets the log level for the underlying boto library
-        get_logger('boto').setLevel( LEVELS[ self.args.boto_log_level ] )
+        get_logger('boto').setLevel( LEVELS[ self.___args.boto_log_level ] )
 
         ### access it via the object
         self.___boto = boto
@@ -116,12 +140,12 @@ class Boto(object):
         ### This also gives us hooks for future logging/timers/etc and
         ### extended wrapping of things the attributes return if we so
         ### choose.
-        self.logger.debug('Calling wrapped boto attribute: %s', attr)
+        self.___logger.debug('Calling wrapped boto attribute: %s', attr)
 
         attr = getattr(self.___boto, attr)
 
         if callable(attr):
-            self.logger.debug("Boto attribute '%s' is callable", attr)
+            self.___logger.debug("Boto attribute '%s' is callable", attr)
 
             @wraps(attr)
             def wrapper(*args, **kwargs):
@@ -150,10 +174,11 @@ class Application(krux.cli.Application):
 
 
 def main():
-    app = Application()
+    app    = Application()
+    region = boto.ec2.get_region(app.boto.cli_region)
+    ec2    = app.boto.connect_ec2(region = region)
 
-    ec2 = app.boto.connect_ec2()
-
+    app.logger.warn('Connected to region: %s', region.name)
     for r in ec2.get_all_regions():
         app.logger.warn('Region: %s', r.name)
 
