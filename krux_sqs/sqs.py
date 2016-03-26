25973b6cdf5932fb66064f1d2d999b9da13fe9bf
Peter Han <phan@krux.com>
2016-03-25 17:26:36 -0700

    Implementing get_sqs()

--- krux_sqs/sqs.py
+++ krux_sqs/sqs.py
@@ -22,7 +22,7 @@ import simplejson
 
 from krux.logging import get_logger
 from krux.stats import get_stats
-from krux.cli import get_group
+from krux.cli import get_parser, get_group
 from krux_boto.boto import Boto3, add_boto_cli_arguments
 
 
@@ -30,7 +30,43 @@ NAME = 'krux-sqs'
 
 
 def get_sqs(args=None, logger=None, stats=None):
-    pass
+    """
+    Return a usable Sqs object without creating a class around it.
+
+    In the context of a krux.cli (or similar) interface the 'args', 'logger'
+    and 'stats' objects should already be present. If you don't have them,
+    however, we'll attempt to provide usable ones for the SQS setup.
+
+    (If you omit the add_sqs_cli_arguments() call during other cli setup,
+    the Boto object will still work, but its cli options won't show up in
+    --help output)
+
+    (This also handles instantiating a Boto3 object on its own.)
+    """
+    if not args:
+        parser = get_parser()
+        add_sqs_cli_arguments(parser)
+        args = parser.parse_args()
+
+    if not logger:
+        logger = get_logger(name=NAME)
+
+    if not stats:
+        stats = get_stats(prefix=NAME)
+
+    boto = Boto3(
+        log_level=args.boto_log_level,
+        access_key=args.boto_access_key,
+        secret_key=args.boto_secret_key,
+        region=args.boto_region,
+        logger=logger,
+        stats=stats,
+    )
+    return Sqs(
+        boto=boto,
+        logger=logger,
+        stats=stats,
+    )
 
 
 def add_sqs_cli_arguments(parser, include_boto_arguments=True):
