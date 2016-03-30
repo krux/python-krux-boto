a78077a26e1ad8c9172b90f5ce5fe6ecc37f3897
Peter Han <phan@krux.com>
2016-03-30 15:50:16 -0700

    Patch version increment

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 
 # URL to the repository on Github.
 REPO_URL = 'https://github.com/krux/python-krux-boto'
